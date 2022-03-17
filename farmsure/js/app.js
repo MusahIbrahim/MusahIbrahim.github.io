95e1e3e0afcc7da4619b31ed82bd58694450e4d7
Musah Ibrahim <mark.ibm.musah@gmail.com>
2022-03-17 18:12:05 +0000

    first version

---
+++ farmsure/js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    //Hero slider
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        smartSpeed:1000,
+        navText:['Prev','Next'],
+        responsive:{
+            0:{
+
+            },
+            600:{
+
+            },
+            1000:{
+
+            }
+        }
+    });
+})
+
+// initializing Aos
+
